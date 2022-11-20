940df5b6b3bb4a7f57da078956fd9b1976934e14
Ahmet Altindis <ahaltindis@gmail.com>
2022-11-20 18:11:50 +0000

    Add domain and certificate to Cloudfront

--- lib/ahmt-me-cdk-stack.ts
+++ lib/ahmt-me-cdk-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 
 export class AhmtMeCdkStack extends cdk.Stack {
@@ -10,6 +12,9 @@ export class AhmtMeCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const sourceBucket = new s3.Bucket(this, 'SourceBucket');
+
+    const ahmtMeCertificateArn = ssm.StringParameter.valueForStringParameter(this, '/ahmtme/certificate/arn');
+    const ahmtMeCertificate = acm.Certificate.fromCertificateArn(this, 'AhmtMeCertificate', ahmtMeCertificateArn);
 
     // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_cloudfront-readme.html#cloudfront-function
     const urlRewriteAppendIndexFunction = new cloudfront.Function(this, 'UrlRewriteAppendIndexFunction', {
@@ -20,6 +25,12 @@ export class AhmtMeCdkStack extends cdk.Stack {
 
     new cloudfront.Distribution(this, 'SourceCDN', {
       priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      certificate: ahmtMeCertificate,
+      domainNames: [
+        'ahmt.me',
+        'www.ahmt.me',
+        'ahmet.altindis.org'
+      ],
       defaultBehavior: {
         origin: new origins.S3Origin(sourceBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
